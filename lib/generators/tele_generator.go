51ea49ad2cb5c6ee05a1c04f0a754b05a19a2588
Tim Storey <tstorey@gameaccount.com>
2022-09-25 21:33:32 +0100

    Add some rudimentary offset index generation for events in eben blocks

--- lib/generators/tele_generator.go
+++ lib/generators/tele_generator.go
@@ -3,15 +3,33 @@ package generators
 import (
 	"fmt"
 	"github.com/lbdl/hexa_tele/lib/types"
+	"math"
 )
 
-func GenerateTelemetry(tele types.TimeLine) {
-	for k, v := range tele.TimeLines {
+type timeLine struct {
+}
+
+func GenerateTelemetry(tl types.TimeLine) {
+	for k, v := range tl.TimeLines {
 		fmt.Println("Generating for: ", k)
 		genData(v)
+		idxs := genOffsets(v)
+		for _, v := range idxs {
+			fmt.Printf("Offset index: %v\n", math.Abs(v))
+		}
 	}
 }
 
 func genData(tl types.TLine) {
 	fmt.Println("Val: ", tl)
+}
+
+func genOffsets(tl types.TLine) []float64 {
+	var offsets []float64
+
+	for _, v := range tl.Events {
+		offset := float64(tl.MaxIntervals) * (v.StartOffset / 100)
+		offsets = append(offsets, offset)
+	}
+	return offsets
 }

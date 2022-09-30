959c6a6484b326b1f4686038280c444d23190eee
Tim Storey <tstorey@gameaccount.com>
2022-09-30 09:20:32 +0100

    Refactoring

--- lib/generators/tele_generator.go
+++ lib/generators/tele_generator.go
@@ -3,6 +3,7 @@ package generators
 import (
 	"fmt"
 	"github.com/lbdl/hexa_tele/lib/types"
+	"golang.org/x/exp/slices"
 	"math"
 	"time"
 )
@@ -30,9 +31,9 @@ func GenerateTelemetry(tl types.TimeLine) {
 	for k, v := range tl.TimeLines {
 		fmt.Println("Generating for: ", k)
 		genData(v)
-		idxs := genOffsets(v)
+		idxs := genEventOffsets(v)
 		for _, v := range idxs {
-			fmt.Printf("Offset index: %v\n", math.Abs(v))
+			fmt.Printf("Offset index: %v\n", v)
 		}
 	}
 }
@@ -41,12 +42,37 @@ func genData(tl types.TLine) {
 	fmt.Println("Val: ", tl)
 }
 
-func genOffsets(tl types.TLine) []float64 {
-	var offsets []float64
-
+func genEventOffsets(tl types.TLine) []eventOffset {
+	var offsets []eventOffset
 	for _, v := range tl.Events {
-		offset := float64(tl.DataPoints) * (v.StartOffset / 100)
-		offsets = append(offsets, offset)
+		offSet := eventOffset{}
+		offSet.startOffset = int(math.Abs((float64(tl.DataPoints) * (v.StartOffset / 100))))
+		offSet.endOffset = int(math.Abs(float64(tl.DataPoints) * (v.EndOffset / 100)))
+		offsets = append(offsets, offSet)
 	}
 	return offsets
+}
+
+// check that we don't have an overlap of offsets
+// if we do then just set the start to the last + 1
+// and note this in the log/stdout/stderr.
+func validateEventStartOffsets(e []eventOffset) {
+	for i, ev := range e {
+		if i > 0 {
+			if ev.startOffset <= e[i].endOffset {
+				ev.startOffset = e[i].endOffset + 1
+			}
+		}
+	}
+}
+
+// if the end index is smaller than the start index just
+// delete the struct from the slice and note this in the log
+// stderr/stdout.
+func validateEventEndOffsets(e []eventOffset) {
+	for i, ev := range e {
+		if ev.startOffset <= ev.endOffset {
+			e = slices.Delete(e, i, i+1)
+		}
+	}
 }
